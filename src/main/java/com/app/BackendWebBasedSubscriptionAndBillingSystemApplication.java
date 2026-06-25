a1b01e688a365b75bcb42fc340efe317b3c2c213
Aryan Raval <aryanraval666@gmail.com>
2026-06-25 23:47:44 +0530

    Initial Commit - Spring Boot backend setup

---
+++ src/main/java/com/app/BackendWebBasedSubscriptionAndBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebBasedSubscriptionAndBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebBasedSubscriptionAndBillingSystemApplication.class, args);
+	}
+
+}
